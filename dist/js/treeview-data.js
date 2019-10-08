4e2c945e5be4a960e7acea2e5ca548bfc1934d04
Rhisen93 <antoniocaprino@kromix.it>
2019-10-08 18:22:14 +0200

    modifiche nella pagina maps

--- dist/js/treeview-data.js
+++ dist/js/treeview-data.js
@@ -5,53 +5,33 @@ $(function() {
 		
         var defaultData = [
           {
-            text: 'Parent 1',
+            text: 'Aree Naturali Tutelate',
             href: '#parent1',
             tags: ['4'],
             nodes: [
               {
-                text: 'Child 1',
+                text: 'Aree Protette',
                 href: '#child1',
                 tags: ['2'],
-                nodes: [
-                  {
-                    text: 'Grandchild 1',
-                    href: '#grandchild1',
-                    tags: ['0']
-                  },
-                  {
-                    text: 'Grandchild 2',
-                    href: '#grandchild2',
-                    tags: ['0']
-                  }
-                ]
-              },
-              {
-                text: 'Child 2',
-                href: '#child2',
-                tags: ['0']
               }
             ]
           },
           {
-            text: 'Parent 2',
+            text: 'Patrimonio Geologico',
             href: '#parent2',
-            tags: ['0']
-          },
-          {
-            text: 'Parent 3',
-            href: '#parent3',
-             tags: ['0']
-          },
-          {
-            text: 'Parent 4',
-            href: '#parent4',
-            tags: ['0']
-          },
-          {
-            text: 'Parent 5',
-            href: '#parent5'  ,
-            tags: ['0']
+            tags: ['2'],
+            nodes: [
+              {
+                text: 'Geositi Areali ',
+                href: '#child3',
+                tags: ['0']
+              },
+              {
+                text: 'Geositi Totali',
+                href: '#child4',
+                tags: ['0']
+              },
+            ]
           }
         ];
 
@@ -142,7 +122,7 @@ $(function() {
         ']';
 
 
-        $('#treeview1').treeview({
+        $('#albero').treeview({
           data: defaultData
         });
 
