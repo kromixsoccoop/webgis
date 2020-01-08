ec51a04f0f4865afe9f82410e78139d80c927d0d
StefanoV1989 <anonymouz89@gmail.com>
2020-01-08 10:38:37 +0100

    Funzioni JS mappa

--- dist/js/maps-api.js
+++ dist/js/maps-api.js
@@ -67,7 +67,7 @@ var ruler1label;
 var ruler2label;
 
 /* MAPS API */
-function righello()
+function righelloOLD()
 {
 	ilRighello = true;
 	$('button#toggleRighello .btn-text').html("NASCONDI RIGHELLO");
@@ -115,7 +115,7 @@ function righello()
 	});
 }
 
-function togliRighello()
+function togliRighelloOLD()
 {
 	google.maps.event.clearListeners(ruler1, "drag");
 	ruler1.setMap(null);
@@ -143,4 +143,266 @@ function distance()
 		var distanza = Math.round(distance/1000);
 		return distanza+"km";
 	}
+}
+
+/* MAPS AVANZATA */
+var listaMarker = Array();
+var listaLabel = Array();
+var listaPosizioniMarker = Array();
+var lineaRighello;
+
+function righello()
+{
+	ilRighello = true;
+	listaMarker = Array();
+	listaLabel = Array();
+	
+	$('button#toggleRighello').html("NASCONDI RIGHELLO");
+	$('button#toggleRighello').attr("onclick", "togliRighello()");
+	
+	lineaRighello = new google.maps.Polyline({
+		//path: listaPosizioniMarker,
+		strokeColor: "#FFFF00",
+		strokeOpacity: .7,
+		strokeWeight: 8
+	});
+	
+	lineaRighello.setMap(map);
+	
+	google.maps.event.addListener(map,'click',function(event)
+	{
+		var posizioneAttuale = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+		
+		var punto = new google.maps.Marker({
+			position: posizioneAttuale,
+			map: map,
+			//icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png',
+			draggable: false
+		});
+		
+		listaMarker.push(punto);
+		
+		label_punto = new Label({ map: map });
+		label_punto.bindTo('position', punto, 'position');
+		label_punto.set('text',"ORIGINE");
+		
+		listaLabel.push(label_punto);
+		
+		
+		
+		if(listaMarker.length > 1)
+		{
+			var distanzaTotale = 0.00;
+			
+			lineaRighello.setMap(null);
+			
+			listaPosizioniMarker = Array();
+			listaPosizioniMarker.push(listaMarker[0].position);
+			
+			for(i=1;i<listaMarker.length;i++)
+			{
+				distanzaTotale += distanzaPunti(listaMarker[(i-1)], listaMarker[i]);
+				listaPosizioniMarker.push(listaMarker[i].position);
+				
+			}
+			
+			lineaRighello.setPath(listaPosizioniMarker);
+			
+			//alert(distanzaTotale);
+			
+			label_punto.set('text',formatDistance(distanzaTotale));
+			
+			
+			lineaRighello.setMap(map);
+			
+			
+		}
+		
+		
+	});
+}
+
+function distanzaPunti(punto1, punto2)
+{
+	
+	var distance = google.maps.geometry.spherical.computeDistanceBetween(punto1.position, punto2.position);
+	
+	return distance;
+}
+
+function formatDistance(distance)
+{
+	if (distance<10000)
+	{
+		return Math.round(distance)+"mt";
+	}
+	else if (distance>=10000)
+	{
+		var distanza = Math.round(distance/1000);
+		return distanza+"km";
+	}
+}
+
+function togliRighello()
+{
+	
+	google.maps.event.clearListeners(map, "click");
+	
+	for(i=0;i<listaMarker.length;i++)
+	{
+		listaMarker[i].setMap(null);
+		listaLabel[i].setMap(null);
+		lineaRighello.setMap(null);
+	}
+	
+	listaMarker = Array();
+	listaLabel = Array();
+	listaPosizioniMarker = Array();
+	
+	ilRighello = false;
+	$('button#toggleRighello').html("RIGHELLO");
+	$('button#toggleRighello').attr("onclick", "righello()");
+}
+
+var lArea = false;
+var listaMarkerArea = Array();
+var listaPosizioniMarkerArea = Array();
+var lineaPoligono;
+var labelArea;
+var puntoLabel;
+
+function mostraArea()
+{
+	var centro = { lat: 0, lng: 0 }
+	lArea = true;
+	listaMarkerArea = Array();
+	var distanza = 0.00;
+	
+	$('button#toggleArea').html("NASCONDI AREA");
+	$('button#toggleArea').attr("onclick", "togliArea()");
+	
+	lineaPoligono = new google.maps.Polygon({
+		//path: listaPosizioniMarker,
+		strokeColor: "#FFFF00",
+		strokeOpacity: .7,
+		strokeWeight: 8
+	});
+	
+	lineaPoligono.setMap(map);
+	
+	labelArea = new Label({ map: map });
+	puntoLabel = new google.maps.Marker({
+		position: centro,
+		map: map,
+		icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 0
+        },
+		draggable: false
+	});
+	labelArea.bindTo('position', puntoLabel, 'position');
+	labelArea.set('text',"");
+	
+	var contaPunti = 0;
+	
+	google.maps.event.addListener(map,'click',function(event)
+	{
+		
+		var posizioneAttuale = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+		
+		var punto = new google.maps.Marker({
+			position: posizioneAttuale,
+			map: map,
+			//icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png',
+			draggable: true,
+			customInfo: contaPunti
+		});
+		
+		listaMarkerArea.push(punto);
+		listaPosizioniMarkerArea.push(punto.position);
+		
+		
+		google.maps.event.addListener(punto, 'drag', function() {
+			listaPosizioniMarkerArea[punto.customInfo] = punto.position;
+			lineaPoligono.setPath(listaPosizioniMarkerArea);
+			lineaPoligono.setMap(map);
+			centro = centroid(listaPosizioniMarkerArea);
+			puntoLabel.setPosition(centro);
+			
+			distanza = google.maps.geometry.spherical.computeArea( listaPosizioniMarkerArea );
+			
+			labelArea.set('text',formatDistance(distanza));
+		});
+
+		
+		
+		
+		
+		if(listaMarkerArea.length > 2)
+		{
+			
+			
+			lineaPoligono.setMap(null);
+
+			lineaPoligono.setPath(listaPosizioniMarkerArea);
+			
+			lineaPoligono.setMap(map);
+			
+			
+			
+			
+			
+			centro = centroid(listaPosizioniMarkerArea);
+			
+			puntoLabel.setPosition(centro);
+			
+			distanza = google.maps.geometry.spherical.computeArea( listaPosizioniMarkerArea );
+			
+			labelArea.set('text',formatDistance(distanza));
+			
+			
+			//labelArea.setMap(null);
+
+
+			
+			
+		}
+		
+		contaPunti++;
+	});
+
+}
+
+function centroid(punti)
+{
+	var bounds = new google.maps.LatLngBounds();
+	
+	for (i = 0; i < punti.length; i++) {
+	  bounds.extend(punti[i]);
+	}
+	
+	var totale = bounds.getCenter();
+	
+	return totale;
+}
+
+function togliArea()
+{
+	google.maps.event.clearListeners(map, "click");
+	
+	for(i=0;i<listaMarkerArea.length;i++)
+	{
+		google.maps.event.clearListeners(listaMarkerArea[i], "drag");
+		listaMarkerArea[i].setMap(null);
+		labelArea.setMap(null);
+		puntoLabel.setMap(null);
+		lineaPoligono.setMap(null);
+	}
+	
+	listaMarkerArea = Array();
+	listaPosizioniMarkerArea = Array();
+	
+	lArea = false;
+	$('button#toggleArea').html("AREA");
+	$('button#toggleArea').attr("onclick", "mostraArea()");
 }
